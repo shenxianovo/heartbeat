bebbc32a37a748751acd8771dc19e904a3387e09
shenxianovo <shenxianovo@outlook.com>
2026-03-01 15:41:12 +0800

    chore: ef core migration, add status fiends in entity Device

---
+++ server/Migrations/20260301073953_AddStatusFields.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentApp",
+                table: "Devices",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastSeen",
+                table: "Devices",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentApp",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "LastSeen",
+                table: "Devices");
+        }
+    }
+}
